4ea608e505ed81ead57180926159532ef33e348d
John Kim <getitdonemin@gmail.com>
2022-08-08 18:42:09 -0500

    More colors added, Local storage data now in use for doughnutchart, Total function fixed

--- indexChart.js
+++ indexChart.js
@@ -1,22 +1,35 @@
 'use strict';
-
-var DEFAULT_COLORS = ['#f08700', '#f49f0a', '#efca08', '#00a6a6', '#bbdef0'];
-
-var total = function(myChart) {
-	var sum = myChart.config.data.datasets[0].data.reduce((a, b) => a + b, 0);
-	return `Total: ${sum}`;
+//the colors of the doughtnut labels
+var labelColors = ['#f08700', '#f49f0a', '#efca08', '#00a6a6', '#bbdef0','#7fb7be', '#357266', '#dacc3e', '#bc2c1a', '#7d1538'];
+//variables made from our localstorage and was pushed into two arrays(Expense & Cost) using a forloop
+var expenseCosts = JSON.parse(localStorage.getItem("list"))
+var expenseLabel = []
+var expenseData = []
+for(var i = 0; i <expenseCosts.length; i++)
+{
+    var costs = expenseCosts[i];
+    expenseLabel.push(costs[0]);
+    expenseData.push(costs[1]);
 }
+//function that gets the sum of the costs data
+var initialValue = 0;
+var total = function(Chart) {
+	var sum = expenseData.reduce(
+		(previousValue, currentValue) => previousValue + Number(currentValue),
+	initialValue)
+	return `Total: ${sum}`;
+};
 // Doughnut with one line of text in the center
 var ctx = document.getElementById('chart').getContext('2d');
 var Chart = new Chart(ctx, {
 	type: 'doughnut',
 	data: {
 		datasets: [{
-			data: [200,300,400,500],
-			backgroundColor: DEFAULT_COLORS,
-			label: 'Dataset 1'
+			data: expenseData,
+			backgroundColor: labelColors,
+			label: expenseData
 		}],
-		labels: ['Item one', 'Item two', 'Item three', 'Item four']
+		labels: expenseLabel
 	},
 	options: {
 		responsive: true,
